1df39c113f6e29271a23fdaf8177a47ddcbc4b28
MaximKrylov <maxim.kryloff@mail.ru>
2017-04-15 10:04:46 +0300

    Added dboardcomp.jsx.

---
+++ src/development/components/dboardcomp.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton';
+
+import FileFolderOpen from 'material-ui/svg-icons/file/folder-open';
+import AvPlayArrow from 'material-ui/svg-icons/av/play-arrow';
+
+import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+class DashboardComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        injectTapEventPlugin();
+    }
+
+    render() {
+        return (
+            <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+                <Toolbar style={{ backgroundColor: "#212121" }}>
+                    <ToolbarGroup>
+                        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+                            <FlatButton label="OPEN" icon={<FileFolderOpen />} />
+                        </MuiThemeProvider>
+                        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+                            <FlatButton label="RUN" icon={<AvPlayArrow />} />
+                        </MuiThemeProvider>
+                    </ToolbarGroup>
+                </Toolbar>
+            </MuiThemeProvider>
+        );
+    }
+}
+
+export default DashboardComponent;
+
--- src/development/components/devcomp.jsx
+++ src/development/components/devcomp.jsx
@@ -3,20 +3,7 @@ import ReactDOM from 'react-dom';
 
 import EditorComponent from './edicomp';
 import TreeComponent from './treecomp';
-
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-
-import FlatButton from 'material-ui/FlatButton';
-import IconButton from 'material-ui/IconButton';
-
-import FileFolderOpen from 'material-ui/svg-icons/file/folder-open';
-import AvPlayArrow from 'material-ui/svg-icons/av/play-arrow';
-
-import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar';
-
-import injectTapEventPlugin from 'react-tap-event-plugin';
+import DashboardComponent from './dboardcomp';
 
 const data = {
     name: 'root',
@@ -55,7 +42,6 @@ class DevelopmentComponent extends React.Component {
         this.state = { value: "// Write your code here..." };
         this.onTreeNodeToggle = this.onTreeNodeToggle.bind(this);
         this.onEditorChange = this.onEditorChange.bind(this);
-        injectTapEventPlugin();
     }
 
     onEditorChange(value) {
@@ -82,18 +68,7 @@ class DevelopmentComponent extends React.Component {
                     <div className="col-xs-3 no-padding">
                         <div className="row">
                             <div className="col-xs-12">
-                                <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-                                    <Toolbar style={{ backgroundColor: "#212121" }}>
-                                        <ToolbarGroup>
-                                            <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-                                                <FlatButton label="OPEN" icon={<FileFolderOpen />} />
-                                            </MuiThemeProvider>
-                                            <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-                                                <FlatButton label="RUN" icon={<AvPlayArrow />} />
-                                            </MuiThemeProvider>
-                                        </ToolbarGroup>
-                                    </Toolbar>
-                                </MuiThemeProvider>
+                                <DashboardComponent/>
                             </div>
                         </div>
                         <div className="row">
