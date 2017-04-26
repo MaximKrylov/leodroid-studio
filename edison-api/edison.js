01540b386616a8f2a224f03d11d79969d870c63d
MaximKrylov <maxim.kryloff@mail.ru>
2017-04-26 21:45:40 +0300

    Refactored the code.

--- edison-api/edison.js
+++ edison-api/edison.js
@@ -1,16 +1,5 @@
 const say = window.require('say');
 
-if (console !== undefined) {
-    if (console.log !== undefined) {
-        const log = console.log;
-
-        console.log = function (message) {
-            log(message);
-            document.write(message);
-        }
-    }
-}
-
 module.exports = {
     say: function (message) {
         say.speak(message);
