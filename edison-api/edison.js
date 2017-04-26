b5cdf1bd3a8ff163119102b0b647946763828f79
MaximKrylov <maxim.kryloff@mail.ru>
2017-04-26 21:14:24 +0300

    Added console.log.

--- edison-api/edison.js
+++ edison-api/edison.js
@@ -1,7 +1,11 @@
 const say = window.require('say');
 
+console.log = function (str) {
+    document.write(str);
+}
+
 module.exports = {
-    say: function(message) {
+    say: function (message) {
         say.speak(message);
     }
 }
