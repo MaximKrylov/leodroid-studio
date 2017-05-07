5451d5e204eada2358aed8adb4a26ab7c190dba2
MaximKrylov <maxim.kryloff@mail.ru>
2017-05-07 12:56:40 +0300

    Refactored leodroid mock.

--- src/app/assets/sample-app/leodroid.js
+++ src/app/assets/sample-app/leodroid.js
@@ -9,139 +9,151 @@ const ent = Crafty.e('2D, DOM, Motion, Image');
 ent.attr({ x: 140, y: 140 })
 ent.acceleration().x = 0
 
-const minX = 0, minY = 0, maxX = 335, maxY = 335;
-const north = 'North', south = 'South', east = 'East', west = 'West';
-const northPic = 'assets/North.png',
-    southPic = 'assets/South.png',
-    eastPic = 'assets/East.png',
-    westPic = 'assets/West.png';
+const minX = 0,
+    minY = 0,
+    maxX = 335,
+    maxY = 335;
+
 const forwardDirectionVelocity = 20,
     backwardDirectionVelocity = -20,
     noDirectionVelocity = 0;
 // ****************************************************************
 
-ent.image(eastPic);
-let direction = east;
+let _direction = null;
+let moved = false, stopped = true;
+
+function setDirection(direction) {
+    switch (direction) {
+        case "North":
+            ent.image("assets/North.png");
+            _direction = "North";
+            break;
+        case "East":
+            ent.image("assets/East.png");
+            _direction = "East";
+            break;
+        case "South":
+            ent.image("assets/South.png");
+            _direction = "South";
+            break;
+        case "West":
+            ent.image("assets/West.png");
+            _direction = "West";
+            break;
+    }
+}
+
+function getDirection() {
+    return _direction;
+}
 
 function move() {
-    switch (direction) {
-        case north:
+    switch (getDirection()) {
+        case "North":
             ent.velocity().x = noDirectionVelocity;
             ent.velocity().y = backwardDirectionVelocity;
             break;
-        case south:
+        case "South":
             ent.velocity().x = noDirectionVelocity;
             ent.velocity().y = forwardDirectionVelocity;
             break;
-        case west:
+        case "West":
             ent.velocity().x = backwardDirectionVelocity;
             ent.velocity().y = noDirectionVelocity;
             break;
-        case east:
+        case "East":
             ent.velocity().x = forwardDirectionVelocity;
             ent.velocity().y = noDirectionVelocity;
             break;
     }
+    
+    moved = true;
+    stopped = false;
 }
-
-ent.bind("Move", function (oldPosition) {
-    if (this.x > maxX && this.y < minY) {
-        direction = south;
-        ent.image('assets/South.png');
-        move();
-    } else if (this.x > maxX && this.y > maxY) {
-        direction = west;
-        ent.image('assets/West.png');
-        move();
-    } else if (this.x < minX && this.y > maxY) {
-        direction = north;
-        ent.image('assets/North.png');
-        move();
-    } else if (this.x < minX && this.y < minY) {
-        direction = east;
-        ent.image('assets/East.png');
-        move();
-    } else if (this.x > maxX) {
-        direction = south;
-        ent.image('assets/South.png');
-        move();
-    } else if (this.y > maxY) {
-        direction = west;
-        ent.image('assets/West.png');
-        move();
-    } else if (this.x < minX) {
-        direction = north;
-        ent.image('assets/North.png');
-        move();
-    } else if (this.y < minY) {
-        direction = east;
-        ent.image('assets/East.png');
-        move();
-    }
-});
 
 function stop() {
     ent.resetMotion();
+    
+    moved = false;
+    stopped = true;
 }
 
 function turnLeft() {
-    switch (direction) {
-        case north:
-            stop();
-            direction = west;
-            ent.image('assets/West.png');
-            move();
-            break;
-        case south:
-            direction = east;
-            ent.image('assets/East.png');
-            move();
-            break;
-        case east:
-            direction = north;
-            ent.image('assets/North.png');
-            move();
-            break;
-        case west:
-            direction = south;
-            ent.image('assets/South.png');
-            move();
-            break;
+    if (moved) {
+        switch (getDirection()) {
+            case "North":
+                setDirection("West");
+                move();
+                break;
+            case "South":
+                setDirection("East");
+                move();
+                break;
+            case "East":
+                setDirection("North");
+                move();
+                break;
+            case "West":
+                setDirection("South");
+                move();
+                break;
+        }
     }
 }
 
 function turnRight() {
-    switch (direction) {
-        case north:
-            stop();
-            direction = east;
-            ent.image('assets/East.png');
-            move();
-            break;
-        case south:
-            stop();
-            direction = west;
-            ent.image('assets/West.png');
-            move();
-            break;
-        case east:
-            stop();
-            direction = south;
-            ent.image('assets/South.png');
-            move();
-            break;
-        case west:
-            stop();
-            direction = north;
-            ent.image('assets/North.png');
-            move();
-            break;
+    if (moved) {
+        switch (getDirection()) {
+            case "North":
+                setDirection("East");
+                move();
+                break;
+            case "South":
+                setDirection("West");
+                move();
+                break;
+            case "East":
+                setDirection("South");
+                move();
+                break;
+            case "West":
+                setDirection("North");
+                move();
+                break;
+        }
     }
 }
 
-function turnRound() {
+ent.bind("Move", function () {
+    if (this.x > maxX && this.y < minY) {
+        setDirection("South");
+        move();
+    } else if (this.x > maxX && this.y > maxY) {
+        setDirection("West");
+        move();
+    } else if (this.x < minX && this.y > maxY) {
+        setDirection("North");
+        move();
+    } else if (this.x < minX && this.y < minY) {
+        setDirection("East");
+        move();
+    } else if (this.x > maxX) {
+        setDirection("South");
+        move();
+    } else if (this.y > maxY) {
+        setDirection("West");
+        move();
+    } else if (this.x < minX) {
+        setDirection("North");
+        move();
+    } else if (this.y < minY) {
+        setDirection("East");
+        move();
+    }
+});
 
-}
+setDirection("North");
+
 
 module.exports = {
     say: function (message) {
